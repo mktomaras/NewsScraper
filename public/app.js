4fdd4b9c6ebbb5849543ae03a7b93373377293cc
Tomaras <mkskoly@gmail.com>
2019-08-29 18:22:25 -0500

    modified modal

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ $("#clearBtn").on("click", function(){
 function getArticles() {
     $.getJSON("/articles", function(data){
         for (var i = 0; i < data.length; i++) {
-            $("#articles").append("<div class='card'><div class='card-body' data-id='" + data[i]._id + "'><h4><a href='" + data[i].link + "'>" + data[i].title + "'</a></h4><button type='button' class='btn btn-success btn-sm' data-toggle='modal' data-target='#exampleModal' id='noteBtn'>Add Note</button></div></div>");
+            $("#articles").append("<div class='card'><div class='card-body' data-id='" + data[i]._id + "'><h4><a target='_blank' href='" + data[i].link + "'>" + data[i].title + "'</a></h4><button type='button' class='btn btn-success btn-sm' data-toggle='modal' data-target='#noteModal' id='noteBtn'>Add Note</button></div></div>");
         }
     });
 }
